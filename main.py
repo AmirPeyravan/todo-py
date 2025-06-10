c340c3263e7016542fbcb12d789ab02362888a44
amirpeyravan <141130613+AmirPeyravan@users.noreply.github.com>
2025-06-10 12:16:44 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import os
 
 DATA_FILE = "data.json"
 
-def load_tasks():
-    if os.path.exists(DATA_FILE):
-        with open(DATA_FILE, "r") as f:
-            return json.load(f)
-    return []
+
 
 def save_tasks(tasks):
     with open(DATA_FILE, "w") as f:
